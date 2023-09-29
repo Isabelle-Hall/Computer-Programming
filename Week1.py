6be09efc18b0f81561ad1fd277249c916fe18ab3
Isabelle-Hall <146032244+Isabelle-Hall@users.noreply.github.com>
2023-09-29 01:41:56 +0100

    Add files via upload

---
+++ Week1.py
@@ -0,0 +1,26 @@
+#1
+print("Hello World!")
+
+#2
+name = input("What is your name? ")
+print("Hello, " + (name))
+
+#3
+tempc =38.4
+tempf =(tempc * 1.8)+32
+print("The temperature in celsius for Yorkshire reached: ", (tempc))
+print("In fahrenheit this is: ", (tempf))
+
+#4
+matches = 609
+bats = 1014
+not_out = 162
+runs = 48426
+batting_average = runs / (bats - not_out)
+print("Geoffrey Boycott's batting average is: ", (batting_average))
+
+#5
+lab_group = 24
+group1 = 113 / 24
+group2 = 175 / 24
+group3 = 12 / 24
