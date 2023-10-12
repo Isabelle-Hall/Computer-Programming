c2d94c8f14d413484eb09ccd467487495e8885e4
Hall <I.Hall4456@student.leedsbeckett.ac.uk>
2023-10-12 11:10:35 +0100

    add file

---
+++ Week 3/Portfolio3.py
@@ -0,0 +1,27 @@
+#1. Modify your greeting program so that if the user does not enter a name (i.e. they just press enter), the program responds "Hello, Stranger!".
+# Otherwise it should print a greeting with their name as before.
+
+#2. Write a program that simulates the way in which a user might choose a password.
+# The program should prompt for a new password, and then prompt again.
+# If the two passwords entered are the same the program should say "Password Set" or similar, otherwise it should report an error.
+
+#3. Modify your previous program so that the password must be between 8 and 12 characters (inclusive) long.
+
+#4. Modify your program again so that the chosen password cannot be one of a list of common passwords, defined thus:
+# BAD_PASSWORDS = ['password', 'letmein', 'sesame', 'hello', 'justinbieber']
+
+#5. Modify your program a final time so that it executes until the user successfully chooses a password.
+# That is, if the password chosen fails any of the checks, the program should return to asking for the password the first time.
+
+#6. Write a program that displays the "Seven Times Table".
+# That is, the result of multiplying 7 by every number from 0 to 12 inclusive. The output might start:
+# 0 x 7 = 0
+# 1 x 7 = 7
+# 2 x 7 = 14
+# and so on.
+
+#7. Modify your "Times Table" program so that the user enters the number of the table they require.
+# This number should be between 0 and 12 inclusive.
+
+#8. Modify the "Times Table" again so that the user still enters the number of the table, but if this number is negative the table is printed backwards.
+# So entering "-7" would produce the Seven Times Table starting at "12 times" down to "0 times"
