77359d49135b5932d85f1d318eb649ec0332ee53
Hall <I.Hall4456@student.leedsbeckett.ac.uk>
2023-10-12 10:03:13 +0100

    Updating Files

--- Week 3/Lab3.py
+++ Week 3/Lab3.py
@@ -1,11 +1,36 @@
 #Boolean Expressions
+print(10 < 100) #True
+print(100 != 100) #False
+print(50 >= 50) #True
 
+age = 19
+print(age > 18) #True
+print(age < 21) #True
+print(age < 31) #True
+
+print("a" < "b") #True
+print("b" < "a") #False
+print("John" < "Terry") #True / J is lower in the alphabet than T
+
+print("john" < "Terry") #False / j is higher in T in Python / capital letters evaluate les-than lower-case
+
+print(5 < 10.2) #True
+#print(5 < "Monty") #Error / not same data type
+#print(5 < "5") #Error / not same data type
 
 #Logical Operators with Expressions
+age = 30
+print(age >=18 and age <= 65) #True
+print(age <18 or age>65) #False
+print(not age > 18) #False
 
+print((age>=18 and age <=65) and (not age==30)) #False / 'not' has higher precedence than 'and'
 
 #Chaining Relational Operators
-
+weight = 140
+height = 164
+print(100 < weight and weight < 200) #True
+print(height > 131 and height < 160) #False
 
 #Membership Testing
 
