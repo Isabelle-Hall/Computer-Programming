c301abfedf64210b32492e8d6e1924d88a74f192
Hall <I.Hall4456@student.leedsbeckett.ac.uk>
2023-10-12 09:43:58 +0100

    Practice Commit

---
+++ Week 3/Lab3.py
@@ -0,0 +1,37 @@
+#Boolean Expressions
+
+
+#Logical Operators with Expressions
+
+
+#Chaining Relational Operators
+
+
+#Membership Testing
+
+
+#If Statement
+
+
+#Else Clause
+
+
+#Elif Clause
+
+
+#Non-Boolean Conditions
+
+
+#Ternary Operator
+
+
+#While and For Loops
+
+
+#Range() Function
+
+
+#Break
+
+
+#Continue
