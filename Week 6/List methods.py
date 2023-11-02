f8e5dccbd6d277b065071bb25d6fe82c9767c75b
Isabelle-Hall <146032244+Isabelle-Hall@users.noreply.github.com>
2023-11-02 10:01:17 +0000

    Add files via upload

--- Week 6/List methods.py
+++ Week 6/List methods.py
@@ -3,3 +3,14 @@ import math
 squares = [4, 9, 16, 25]
 for n in squares:
     print(int(math.sqrt(n)))
+
+help(list)
+
+squares.append(36)
+squares.append(49)
+squares.append(64)
+squares.append(81)
+
+
+
+
