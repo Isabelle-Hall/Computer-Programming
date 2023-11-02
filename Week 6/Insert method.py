a289b69a976007d801edc69a93808e9864005f53
Isabelle-Hall <146032244+Isabelle-Hall@users.noreply.github.com>
2023-11-02 10:04:54 +0000

    Add files via upload

--- Week 6/Insert method.py
+++ Week 6/Insert method.py
@@ -1,8 +1,9 @@
 import math
 
 squares = [4, 9, 16, 25]
+squares.insert(0, 2)
+#first argument is position, second is value
 for n in squares:
     print(int(math.sqrt(n)))
 
-squares.insert(0, 2)
-#first argument is position, second is value
+
