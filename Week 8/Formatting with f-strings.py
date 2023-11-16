0d3b054360c3ca17f500476e10d34fd5a109ba8d
Isabelle-Hall <146032244+Isabelle-Hall@users.noreply.github.com>
2023-11-16 09:15:19 +0000

    Add files via upload

---
+++ Week 8/Formatting with f-strings.py
@@ -0,0 +1,16 @@
+name = "Donald"
+addr = "1600 Pennsylvania Ave"
+
+# Expressions are embedded in the string
+print(f"Your name is {name}, and your address is {addr}")
+
+import math
+r = 50
+print(f"A circle with radius {r} has an area of {math.pi * r * r}")
+
+
+width = 104.32
+width = float(width)
+height = 15.645
+height = float(height)
+print(f"The area of a rectangle with a width of {width} and a height of {height} is {width * height}")
